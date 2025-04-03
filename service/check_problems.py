7fc1b4cbb0d4ecfaa752e40acd0f677d3a156574
Vitalizzare <vitalizzare@gmail.com>
2025-04-03 18:06:46 +0300

    updated service/check_problems.py

--- service/check_problems.py
+++ service/check_problems.py
@@ -16,17 +16,18 @@ def check_known_problems(file_name: str) -> dict[tuple[int, str], list[Problem]]
     
     problems = defaultdict(list)
     with open(file_name) as file:
-        for n, line in enumerate(map(lambda line: line.rstrip('\n'), file)):
-            if line.startswith(' '): 
-                problems[(n, line)].append(Problem.leading_space.name)
-            if line.endswith(' '):
-                problems[(n, line)].append(Problem.trailing_space.name)
-            if '\t' not in line and line.strip():
-                problems[(n, line)].append(Problem.no_tab.name)
-            if len(line.split('\t')) > 2:
-                problems[(n, line)].append(Problem.multiple_tab.name)
-            if any(not p.startswith(' ') for p in line.split(',')[1:]):
-                problems[(n, line)].append(Problem.no_space_after_comma.name)
+        data = [line.rstrip('\n') for line in file]
+    for n, line in enumerate(data, start=1):
+        if line.startswith(' '): 
+            problems[(n, line)].append(Problem.leading_space.name)
+        if line.endswith(' '):
+            problems[(n, line)].append(Problem.trailing_space.name)
+        if '\t' not in line and line.strip():
+            problems[(n, line)].append(Problem.no_tab.name)
+        if len(line.split('\t')) > 2:
+            problems[(n, line)].append(Problem.multiple_tab.name)
+        if any(not p.startswith(' ') for p in line.split(',')[1:]):
+            problems[(n, line)].append(Problem.no_space_after_comma.name)
     return problems
 
 
